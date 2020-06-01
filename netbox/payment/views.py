91939c5984c5d54deb46da485251b15499cade76
Alexandr Ovsyannikov <ovs.alexandr@gmail.com>
2020-06-01 14:09:01 +0300

    scripted PC

--- netbox/payment/views.py
+++ netbox/payment/views.py
@@ -330,7 +330,7 @@ class ReportView(View, PermissionRequiredMixin):
     permission_required = 'payment.view_payment'
     queryset = Payment.objects.all()
 
-    def get(self):
+    def get(self, request):
         headers = self.queryset.model.csv_headers.copy()
         for obj in self.queryset:
             data = obj.to_csv()
